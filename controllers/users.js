f3e4b0ae8cb415f1633990c8965721881df5886d
Nolo Marsh <nolo.marsh@gmail.com>
2021-08-06 16:40:49 -0400

    established server, wrote users model, implemented basic CRUD for users

---
+++ controllers/users.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcrypt')
+const express = require('express')
+const router =  express.Router()
+const User = require('../models/users.js')
+
+//Create(post)
+router.post('/', (req,res) => {
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+    User.create(req.body, (error, newUser) => {
+        res.json(newUser)
+    })
+})
+
+//Read(get)
+router.get('/', (req,res) => {
+    User.find({}, (error, allUsers) => {
+        res.json(allUsers)
+    })
+})
+
+//Update(put)
+router.put('/:id', (req,res) => {
+    User.findByIdAndUpdate(req.params.id, req.body, {new:true}, (error, updatedUser) => {
+        res.json(updatedUser)
+    })
+})
+
+//Destroy(delete)
+router.delete('/:id', (req,res) => {
+    User.findByIdAndRemove(req.params.id, (error, removedUser) => {
+        res.json(removedUser)
+    })
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,54 @@
+//******************
+//** Dependencies **
+//******************
+const express = require('express')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const cors = require('cors')
+const validator = require('validator')
+const bcrypt = require('bcrypt')
+require('dotenv').config()
+
+const app = express()
+//******************
+//* Database Setup *
+//******************
+const PORT = process.env.PORT || 3003
+const MONGODB_URI = process.env.MONGODB_URI
+const db = mongoose.connection
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'))
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI))
+db.on('disconnected', () => console.log('mongo disconnected'))
+
+//****************
+//** Middleware **
+//****************
+app.use(cors())
+app.use(express.json())
+
+//*****************
+//** Controllers **
+//*****************
+const usersController = require('./controllers/users.js')
+app.use('/users', usersController)
+
+//****************
+//**** Routes ****
+//****************
+app.get('/', (req,res) => {
+    res.redirect('/users')
+})
+
+//****************
+//*** Listener ***
+//****************
+app.listen(PORT, () => {
+    console.log("Listening on port:", PORT)
+})
